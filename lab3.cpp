975d0a1ae9ba084018c8bff749abe7b09f02e640
Bedadha <45728023+Bedadha@users.noreply.github.com>
2019-08-14 16:08:40 +0530

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,133 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int noofstrline(string expr){
+    int count=0;
+    for(int i=0;i<expr.length(); i++)
+    {
+        if(expr[i]=='|')
+        count++;
+    }
+    return count;
+}
+
+// function to check if paranthesis are balanced 
+bool areParanthesisBalanced(string expr) 
+{ 
+	stack<char> s; 
+	char x;
+	int count=noofstrline(expr);
+	if(count%2==1)
+	return false;
+	int t=0;
+
+	// Traversing the Expression 
+	for (int i=0; i<expr.length(); i++) 
+	{ 
+	    if(expr[i]=='|'){
+	        t++;
+	    }
+		if (expr[i]=='('||expr[i]=='['||expr[i]=='{'||expr[i]=='<'||(expr[i]=='|'&&t<=count/2)) 
+		{ 
+			// Push the element in the stack 
+			s.push(expr[i]); 
+			continue; 
+		}
+
+		// IF current current character is not opening 
+		// bracket, then it must be closing. So stack 
+		// cannot be empty at this point. 
+		if (s.empty()) 
+		return false; 
+
+		switch (expr[i]) 
+		{ 
+		case ')': 
+
+			// Store the top element in a 
+			x = s.top(); 
+			s.pop(); 
+			if (x=='{' || x=='['||x=='|'||x=='<') 
+				return false; 
+			break; 
+
+		case '}': 
+// Store the top element in b 
+			x = s.top(); 
+			s.pop(); 
+			if (x=='(' || x=='['||x=='|'||x=='<') 
+				return false; 
+			break; 
+
+		case ']': 
+
+			// Store the top element in c 
+			x = s.top(); 
+			s.pop(); 
+			if (x =='(' || x == '{'||x=='|'||x=='<') 
+				return false;
+				break;
+				
+	
+	    	case '>': 
+
+			// Store the top element in b 
+			x = s.top(); 
+			s.pop(); 
+			if (x=='(' || x=='['||x == '{'||x=='|') 
+				return false; 
+			break;  
+				case '|': 
+
+			// Store the top element in b 
+			x = s.top(); 
+			s.pop(); 
+			if (x=='(' || x=='['||x == '{'||x=='<') 
+				return false; 
+			break;  
+	
+		} 
+	} 
+
+	// Check Empty Stack 
+	return (s.empty()); 
+} 
+
+// Driver program to test above function 
+int main() 
+{
+	int t;
+	cin>>t; 
+	string expr[t];
+	for(int j=0;j<t;j++)
+	{
+	    cin>>expr[j];
+	}
+	for(int i=0;i<t;i++)
+{
+	if (areParanthesisBalanced(expr[i])) 
+		cout << "YES"; 
+	else
+		cout << "NO"; 
+}
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
